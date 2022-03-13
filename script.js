142d75870f9c99cc51499882104c7fea06230639
Dren Sokoli <drensokoli@gmail.com>
2022-03-13 02:43:08 +0100

    Designed the contact form

--- script.js
+++ script.js
@@ -2,48 +2,16 @@ document.getElementById("button").addEventListener("click", function(){
     document.querySelector(".alert").style.display  = "none";
   })
 
-
-
-
-// Get data
-const nameInput = document.querySelector("#name");
-const surname = document.querySelector("#surname");
-const email = document.querySelector("#email");
-const message = document.querySelector("#message");
-const success = document.querySelector("#success");
-const errorNodes = document.querySelectorAll(".error");
-
-//Validate data
-
-function validateForm(){
-
-    clearMessages();
-
-    if(nameInput.value.length < 1){
-        errorNodes[0].innerText = "Name cannot be blank";
-        nameInput.classList.add("error-border");
-    }
-
-    if(surname.value.length < 1){
-        errorNodes[1].innerText = "Last name cannot be blank";
-        surname.classList.add("error-border");
-    }
-
-    if(!emailIsValid(email.value)){
-        errorNodes[2].innerText = "Invalid email address";
-        email.classList.add("error-border");
-    }
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "drensokoli@gmail.com",
+        Password : "babinani",
+        To : 'ds51318@ubt-uni.net',
+        From : document.getElementById("email").value,
+        Subject : "New contact form Enquiry",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
 }
-
-function clearMessages(){
-    for(let i = 0; i< errorNodes.length; i++){
-        errorNodes[i].innerText = "";
-    }
-    nameInput.classList.remove("error-border");
-}
-
-function emailIsValid(email){
-    let pattern
-}
-
-

f4148b5859c5e8224782712f666837df0f351dc5
Dren Sokoli <drensokoli@gmail.com>
2022-03-13 20:00:23 +0100

    Contact page
    Made contact page responsive and made progress on validating the form

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
 document.getElementById("button").addEventListener("click", function(){
     document.querySelector(".alert").style.display  = "none";
   })
-
-function sendEmail(){
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "drensokoli@gmail.com",
-        Password : "babinani",
-        To : 'ds51318@ubt-uni.net',
-        From : document.getElementById("email").value,
-        Subject : "New contact form Enquiry",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}

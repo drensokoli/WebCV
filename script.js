39e8e7eed24f3f903d42a6e317e9062006944802
Dren Sokoli <drensokoli@gmail.com>
2022-03-14 17:00:29 +0100

    Paralax js

--- script.js
+++ script.js
@@ -4,19 +4,48 @@ document.getElementById("button").addEventListener("click", function(){
 
   const popup = document.querySelector('.alert');
   const close = document.querySelector('.button');
-  const greeting_card = document.querySelector(".card");
+  var greeting_card = document.getElementsByClassName("card");
+
+  function welcome(){
+    document.getElementById("card").style.transform = "translateY(0)";
+  }
+  function start(){
+    document.getElementById("card").style.transition = "3000";
+    window.setTimeout(welcome, 1500)
+  }
+
+
+
+  // function change(){
+
+  //   greeting_card.classList.add("transformed-state");
+
+  //   //   const elements = document.getElementById("card");
+  //   //   elements.toggle("transformed-state");
+  //   //    for (let element of elements) {
+  //   //    element.classList.toggle("transformed-state");
+  //   //  }
+  // }
 
   window.onload = function(){
     setTimeout(function(){
       popup.style.display = "block";
 
     }, 3000)
-    setTimeout(function(){
-      // document.getElementById("card").style.transitionDuration = "2000ms";
-      greeting_card.style.display = "flex";      
-    }, 1500)
+
+    // change({
+
+    // }, 1000)
+
+
+
+
+    start({
+
+    }, )
+
   }
 
   close.addEventListener('click', () => {
     popup.style.display = "none";    
-  })
+  }) 

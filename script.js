6fae57a2756366cef6438a50cd396d51ba909324
Dren Sokoli <drensokoli@gmail.com>
2022-03-14 03:25:51 +0100

    Fixed the popup with .js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ document.getElementById("button").addEventListener("click", function(){
     document.querySelector(".alert").style.display  = "none";
   })
 
+  const popup = document.querySelector('.alert');
+  const close = document.querySelector('.button');
+
+  window.onload = function(){
+    setTimeout(function(){
+      popup.style.display = "block";
+
+    }, 3000)
+  }
+
+  close.addEventListener('click', () => {
+    popup.style.display = "none";    
+  })
